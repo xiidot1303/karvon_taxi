73dec1f22aa962073f435dc3c1047a5b34139951
xiidot1303 <xii1303@inbox.ru>
2023-05-17 06:46:50 +0500

    fix transaction category

--- app/services/transaction_category_service.py
+++ app/services/transaction_category_service.py
@@ -3,10 +3,10 @@ from app.models import Yandex_transaction_category as Category
 
 def get_or_create_transaction_category(id, name):
     category_obj, created = Category.objects.get_or_create(category_id=id)
-    category_obj.name = name
+    category_obj.name = name.lower()
     category_obj.save()
     return category_obj
 
 def get_transaction_category_id_by_name(name):
-    query = Category.objects.filter(name__icontains=name)
+    query = Category.objects.filter(name=name)
     return query[0].category_id if query else None
