e4f676a7432cd9e7e541bf41794bdf346b72b110
xiidot1303 <xii1303@inbox.ru>
2023-08-08 09:20:33 +0500

    fix admin of sms

--- sms/admin.py
+++ sms/admin.py
@@ -2,7 +2,17 @@ from django.contrib import admin
 from sms.models import *
 
 class NewsletterAdmin(admin.ModelAdmin):
-    list_display = ['driver', 'text', 'datetime']
+    list_display = ['drivers_name', 'text', 'datetime']
+
+    def drivers_name(self, obj):
+        result = ''
+        if users:=obj.get_drivers:
+            for user in users:
+                result += f'{user.first_name} {user.phone} | '
+        else:
+            result = 'All'
+        return result
+
 
 class Alert_by_last_orderAdmin(admin.ModelAdmin):
     list_display = ['since', 'text']
